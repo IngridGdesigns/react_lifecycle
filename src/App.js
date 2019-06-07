07c928383e497ecfc9777a1dc0d2ae06a636eaf5
Ingrid G <aleja.morada@gmail.com>
2019-06-07 11:40:02 -0700

    Added Component did mount with studio ghibli api endpoint

--- src/App.js
+++ src/App.js
@@ -27,17 +27,37 @@ class App extends React.Component {
 
 class Content extends React.Component {
 
+  //Is invoked immediatley before mounting occurs, It is called
+  //before render() therefore setting state in this method will
+  //not trigger a re-render Do not may async calls here for initialization!!!!
    componentWillMount() {
       console.log('Component WILL MOUNT!')
+    
+   }
 
+   //Make async calls for component initialization in componentDidMount
+   componentDidMount() {
+    const headers ={
+      method: 'GET',
+      body: JSON.stringify(),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    console.log('Component Did Mount!!!! :D')
+     fetch(`https://ghibliapi.herokuapp.com/films`, headers)
+     .then(res => res.json())
+     .then(res => console.log(res))
    }
 
 
   
    render() {
+   
       return (
          <div>
             <h3>{this.props.myNumber}</h3>
+            
          </div>
       );
    }
