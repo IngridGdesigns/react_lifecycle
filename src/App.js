f3db9efbb2c3d8b1e37c2611e9f00142aacabbd4
Ingrid G <aleja.morada@gmail.com>
2019-06-10 10:58:33 -0700

    Added extra notes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+/*
+resources: 
+https://blog.pusher.com/beginners-guide-react-component-lifecycle/
+https://www.wikitechy.com/tutorials/react/reactjs-component-life-cycle
+
+shouldComponentUpdate is a great place to improve the performance of a component 
+because it can help to prevent unnecessary re-rendering. However, it is advised not 
+to use this method for deep equality checks or JSON.stringify as this is very 
+inefficient and may harm performance.
+*/
+
+
 import React from 'react';
 import './App.css'
 
@@ -69,7 +81,7 @@ class Content extends React.Component {
    }
 
    //Make async calls for component initialization in componentDidMount
-//    componentDidMount is executed after first render only on the client side.
+// componentDidMount is executed after first render only on the client side.
 // This is where AJAX requests and DOM or state updates should occur.
 // This method is also used for integration with other JavaScript frameworks and any functions with delayed execution like setTimeoutor setInterval.
 // We are using it to update the state so we can trigger the other lifecycle methods.
@@ -97,9 +109,14 @@ class Content extends React.Component {
      console.log('Component Will receive the PROPS')
    }
 
-   //whenever React is rendering the view it checks to see if shouldComponentUpdate is 
-   //returning false/true. So whenever you have a component that’s static do yourself a 
-   //favor and return false. Or if is not static check to see if the props/state has changed.
+  //  shouldComponentUpdate is used to let React know that a component’s output is not 
+  //  affected by a change of props or state in the component and thus should not re-render. 
+  //  It returns either a true or false value. If it returns true, the component will go ahead 
+  //  and do what it always does, re-render the component. If it returns false then the component 
+  //  will not update. Note that this does not prevent child components from re-rendering when their state changes.
+
+  //  The best way to use this method is to have it return false and hence the component will not 
+  //  update under certain conditions. If those conditions are met, then the component does not update.
    shouldComponentUpdate(newProps, nextState) {
      console.log('Should component UPDATE? YES...button was pressed, update count')
      return true
@@ -108,7 +125,8 @@ class Content extends React.Component {
 // There is nothing different about this render call compared to the call for the initial render.
 // This point is very important to React.
 // Your render function should not differentiate whether this is the initial render or an update.
-// This is the heart of React takes render's return value and compares it to the return value last time render was called and decides what updates to make, if any.
+// This is the heart of React takes render's return value and compares it to the return value last 
+//time render was called and decides what updates to make, if any.
 // The reconciliation process that we'll dive into next chapter.
 // After reconciliation, React will recursively mount, update, and unmount child components as needed.
 // Once the process resolves to base virtual DOM components, React updates the actual DOM
@@ -116,12 +134,23 @@ class Content extends React.Component {
      console.log('Component WILL UPDATE, press button to see')
    }
 
+   /*
+   componentDidUpdate is called after any rendered HTML has finished loading. It receives two arguments, 
+   the props and state of the component before the current updating period began.
+componentDidUpdate is the best place to perform interaction with non-React environment like the browser 
+or making HTTP requests. This should be done as long as you compare the 
+current props to the previous props to avoid unnecessary network requests.
+   */
    componentDidUpdate(prevProps, prevState) {
      console.log('Component DID UPDATE, new count... :)')
    }
 
 
-//    componentWillUnmount is called after the component is unmounted from the dom. We are unmounting our component in main.js.
+//    componentWillUnmount is called after the component is unmounted from the dom.
+// This is where you can perform any cleanups that should be done such as invalidating timers, 
+// canceling network requests, removing event listeners or canceling any subscriptions made in 
+// componentDidMount.
+//We are unmounting our component in app.js.
 // In our example we are setting initial state in constructor function.
 // The setNewnumber is used to update the state.
 // All the lifecycle methods are inside Content component.
